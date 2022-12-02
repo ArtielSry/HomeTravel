79dcf77239cc5c4b9aefa443b94d65a5ae0ba25c
Soraya Artiel <artielsoraya@gmail.com>
2022-12-02 16:50:08 +0100

    deploying

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,4 @@ export { default as About } from './about/About';
 export { default as Travel } from './travel/Travel';
 export { default as Contact } from './contact/Contact';
 export { default as Discover } from './discover/Discover';
-export { default as Home } from './home/Home';
+export { default as HomeTravel } from './home/Home';
